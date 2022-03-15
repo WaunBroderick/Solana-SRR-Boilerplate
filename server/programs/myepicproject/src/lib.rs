96199369b514f9af79b389305a6abcd5bcb69ad7
Waun Broderick <waunbroderick@gmail.com>
2022-03-15 18:27:41 -0400

    added rust wallet test server to repo

---
+++ server/programs/myepicproject/src/lib.rs
@@ -0,0 +1,31 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod myepicproject {
+  use super::*;
+  pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
+    // Get a reference to the account.
+    let base_account = &mut ctx.accounts.base_account;
+    // Initialize total_gifs.
+    base_account.total_gifs = 0;
+    Ok(())
+  }
+}
+
+// Attach certain variables to the StartStuffOff context.
+#[derive(Accounts)]
+pub struct StartStuffOff<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+// Tell Solana what we want to store on this account.
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
